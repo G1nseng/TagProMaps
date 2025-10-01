1305830e24bee2c0f0646804a08f721cf7227014
Chris Plowman <chrisplowman@Chriss-MacBook-Air.local>
2025-10-01 14:12:42 -0400

    Convert frontend from submodule to normal folder

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className="bg-blue-500 p-4 flex justify-between">
+      <Link to="/" className="text-white font-bold text-xl">
+        TagProMaps
+      </Link>
+      <div className="flex">
+      <Link to="/leaderboard" className="text-white hover:text-gray-300 ml-auto">
+        Leaderboard
+      </Link>
+      <Link to="/inspiration" className="text-white hover:text-gray-300 ml-4">
+        EMPTY
+      </Link>
+      <Link to="https://github.com/cmgchess/facemash-v2" target="_blank" className="text-white hover:text-gray-300 ml-4">
+        EMPTY
+      </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
